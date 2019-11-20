9322f8b12dff3aaa5949fbb6de1c7da0285874b5
Noah <noah.willaime@gmail.com>
2019-11-20 02:39:37 +0100

    envrimnt prod backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
 export const environment = {
-  production: true
+  production: true,
+  backend: {
+    protocol: 'http',
+    host: '0.0.0.0',
+    port: '3000',
+    endpoints: {
+      allAnimals: '/animals',
+      oneAnimal: '/animals/:id',
+      allSpecies: '/animals/species',
+      oneSpecies: '/animals/species/:species',
+      login: '/auth/login',
+      profile: '/profile',
+      allRefuges: '/refuge',
+      oneRefuge: '/refuge/:id',
+      refugeByUser: '/refuge/user/:id',
+      someAnimals: '/refuge/:id/animals',
+      allUser: '/benevoles',
+      oneUser: '/benevoles/:id'
+    }
+  }
 };
